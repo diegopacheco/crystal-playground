4514f5cb442ef1856da2c8d4e79cbac74eacc7ca
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-09 00:49:05 -0700

    crystal lang - literals

--- literals/tuples.cr
+++ literals/tuples.cr
@@ -4,3 +4,7 @@ puts tuple
 puts tuple[0]
 puts tuple[1]
 puts tuple[2]
+
+named_tuple = {name: "Crystal", year: 2011} # NamedTuple(name: String, year: Int32)
+puts named_tuple[:name]
+puts named_tuple[:year]
