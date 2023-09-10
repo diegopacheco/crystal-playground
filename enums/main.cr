d144a7d7332cf41dffa80335943d7851c3476ee8
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-09 21:33:39 -0700

    crystal lang - enums

--- enums/main.cr
+++ enums/main.cr
@@ -6,3 +6,17 @@ end
 
 blue = Color::Blue
 puts blue
+
+enum Level
+  Easy   = 1
+  Medium = 3
+  Hard   = 5
+  Impossible = 7    
+end
+
+lvl = Level::Impossible
+if (Level::Impossible==lvl)
+    puts "level is #{lvl.value}"
+else
+    puts "oops"
+end    
