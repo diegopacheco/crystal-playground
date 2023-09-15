e0b5f698c1acd88063bb76a61826c70ba2d7e086
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-15 02:20:07 -0700

    strings in crystal

---
+++ cli-app/strings/main.cr
@@ -0,0 +1,10 @@
+message = "Hello World! Greetings from Crystal."
+
+puts "normal: #{message}"
+puts "upcased: #{message.upcase}"
+puts "downcased: #{message.downcase}"
+puts "camelcased: #{message.camelcase}"
+puts "capitalized: #{message.capitalize}"
+puts "reversed: #{message.reverse}"
+puts "titleized: #{message.titleize}"
+puts "underscored: #{message.underscore}"
